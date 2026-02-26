73ca9b65094cefa277ba4dec17394f472880caa3
Chris Morris <cmorris.morris@gmail.com>
2026-02-26 10:01:35 -0600

    ci: optimize workflow with dbt build

--- dbt/freight_analytics/macros/generate_schema_name.sql
+++ dbt/freight_analytics/macros/generate_schema_name.sql
@@ -1,9 +1,10 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-        {{ default_schema }}
+    {# If we specify 'public', we want it to stay 'public' #}
+    {%- if custom_schema_name == 'public' -%}
+        public
+    {%- elif custom_schema_name is none -%}
+        {{ target.schema }}
     {%- else -%}
         {{ custom_schema_name | trim }}
     {%- endif -%}
 {%- endmacro %}
-
