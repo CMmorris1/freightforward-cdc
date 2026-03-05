2ed50b10ec624ed4dffd1e36de7f1ec0d44130e3
Chris Morris <cmorris.morris@gmail.com>
2026-03-05 07:10:12 -0600

    Cleaned coments and added diagrams to README files

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "sw-concepts-s3-bucket"
-    key            = "projects/my-app/terraform.tfstate" # Path within the bucket
-    region         = "us-east-1"                          # Use your bucket's region
-    encrypt        = true
-  }
-}
